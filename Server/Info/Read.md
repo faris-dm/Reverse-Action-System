f022098582a83c00404fca9e06c949af3735a94e
faris Naser <solonaser9@gmail.com>
2026-01-25 16:38:57 -0800

    feat:added  the steps in the md file and corrected the method and the type in login

---
+++ Server/Info/Read.md
@@ -0,0 +1,23 @@
+i am using map to store user in fo in my loca
+
+<h2>Register Page</h2>
+
+<p>Pleases Register Now</p>
+<form action="/register" method="POST">
+  <div>
+    <label for="name">Username</label>
+    <input type="text" name="name" class="name" required />
+  </div>
+
+  <div>
+    <label for="email">email</label>
+    <input type="email" name="email" id="email" required />
+  </div>
+
+<label for="password">password</label>
+<input type="password" name="password" id="password" required />
+<button type="submit">
+<a href="/login" class="">Login</a>
+</button>
+
+</form>
---
+++ Server/needed.js
@@ -0,0 +1,77 @@
+// userStore.set(1, { name: "faris", age: 20 });
+// userStore.set(2, { name: "Mahamed", age: 20 });
+// userStore.set(3, {
+//   username: "admin",
+//   email: "admin23@gmail.com",
+//   password: "12345678",
+// });
+
+// console.log(userStore.get(1));
+// let id = 1;
+// app.post("/product", (req, res) => {
+//   let NewProduct = { id, name: req.body.name, age: req.body.age };
+//   userStore.set(id, NewProduct);
+//   id++;
+//   res.send(NewProduct);
+// });
+
+// app.get("/product", (req, res) => {
+//   res, send(Array.from(userStore.values()));
+//   console.log("done sccefully");
+// });
+
+// app.post("/signup", async (req, res) => {
+//   let { username, email, password } = req.body;
+//   if (!email || !password) {
+//     return res.status(400).send("email and pssword are required");
+//   }
+//   if (userStore.has(email) || userStore.has(password)) {
+//     res.status(409).send("pleases choose another username or password");
+//   }
+//   try {
+//     let hashedPassword = await bcrypt.hash(password);
+
+//     let userRole = email === "admin23@gmail.com" ? "admin" : "user";
+//     let newUser = {
+//       username: req.body.username,
+//       role: userRole,
+//       password: hashedPassword,
+//       createdAt: new Date(),
+//     };
+//     userStore.set(email, newUser);
+//     return res.status(201).json({
+//       message: "regsitored succefully",
+//       role: userRole,
+//     });
+//   } catch (error) {
+//     return res.status(500).json({ error: "intrial error" });
+//   }
+// });
+// const jet = require("jsonwebtoken");
+
+// let data = [
+//   { username: "solo", title: "post" },
+//   { username: "admin", title: "read me" },
+// ];
+// app.get("/post", authication, (req, res) => {
+//   res.json(data.filter((item) => item.username === req.user.username));
+// });
+
+// app.post("/login", (req, res) => {
+//   let username = req.body.username;
+//   let user = { name: username };
+//   let accessToken = jet.sign(user, process.env.ACCESS_TOKEN_SECRET);
+//   res.json({ accessToken: accessToken });
+// });
+
+//function authication(req, res, next) {
+// header--Bearerlet
+//   let authHeader = req.headers["authorization"];
+//   let token = authHeader && authHeader.split("")[1];
+//   if (token == null) return res.send("unmatch tokens");
+//   jet.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//     if (err) return res.send("error hapend");
+//     req.user = user;
+//     next();
+//   });
+// }
