22734d35ceca4545bbb2d266875fa8887773f2f4
faris Naser <solonaser9@gmail.com>
2026-01-28 16:53:32 -0800

    feat:created  a function that accepts the input and check validation in the server

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const passport = require("passport");
 let app = express();
+let LocalStrategy = require("passport-local").Strategy;
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -17,6 +19,27 @@ app.get("/login", (req, res) => {
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
+
+let userData = [];
+
+async function authticateUser(email, password, done) {
+  let user = userData.find((item) => item.email === email);
+  if (!user) {
+    return done(null, false, {
+      message: " there is noe user  with this email",
+    });
+  } else {
+    try {
+      if (await bcrypt.compare(password.userData.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "Incorect Password" });
+      }
+    } catch (error) {
+      return done(error);
+    }
+  }
+}
 
 let port = 3000;
 
