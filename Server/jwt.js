9df03ab681b732c4094ecc10723618f07b7f84b2
faris Naser <solonaser9@gmail.com>
2026-02-03 04:52:54 -0800

    feat:created a const  that accept payload and secrket key and change then to token  uaing jwt.sign

--- Server/jwt.js
+++ Server/jwt.js
@@ -2,17 +2,35 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
 let port = 7800;
+app.use(express.json());
+app.set("view engine", "ejs");
 
-app.post("/", (req, res) => {
+app.get("/login", (req, res) => {
   const data = {
     name: "solo Naser",
     age: "32",
     job: "software dev  who  not scrifiess anythings",
     hobbies: "Connecting with people",
   };
-
   res.send(data);
 });
+
+const payload = { sub: "12345", role: "user" };
+// payload:data/sub:subhect
+let secret = "W$q4=25*8%v-}UV";
+const token = jwt.sign(payload, secret, {
+  expiresIn: "14m",
+  //   sign marget the two to create a  token
+});
+
+console.log("accses Token is ", token);
+
+// app.post("/login", (req, res) => {
+//   let username = req.body.name;
+//   const user = { name: username };
+//   const userToken = jwt.sign(user, secret);
+//   res.json({ userToken: userToken });
+// });
 
 app.listen(port, () => {
   console.log(`Server Running on https://localhost:${port}`);
