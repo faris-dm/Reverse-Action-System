a173b4186982baa981adaf4349184a5b5030cc42
faris Naser <solonaser9@gmail.com>
2026-02-08 04:46:07 -0800

    created  new  file to create a jwt auth  and integrate it with routes

---
+++ Server/simpleJwt.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+let cookiesparser = require("cookie-parser");
+app.use(cookiesparser());
+let flash = require("express-flash");
+let userMapStore = new Map();
+const bcrypt = require("bcrypt");
+const { name } = require("ejs");
+const { populate } = require("dotenv");
+app.use(express.urlencoded({ extended: true }));
+
+// app.get("/", (req, res) => {
+//   res.render("index.ejs", { message: name });
+// });
+// app.get("/register", (req, res) => {
+//   res.render("register.ejs");
+// });
+
+// app.get("/login", (req, res) => {
+//   res.render("login.ejs");
+// });
+
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+let secret = "W$q4=25*8%v-}UV";
+
+// app.post("/register", async (req, res) => {
+//   try {
+//     let { email, password } = req.body;
+//     let cleanEmail = email.trim().toLowerCase();
+//     if (userMapStore.has(cleanEmail)) {
+//       alert("email aready exist");
+
+//       return res.redirect("/register");
+//     }
+
+//     let hashedPassword = await bcrypt.hash(password, 10);
+//     userMapStore.set(email, {
+//       id: Date.now(),
+//       name: req.body.name,
+//       email: email,
+//       password: hashedPassword,
+//     });
+//     console.log("stored succesfully", userMapStore.get(email));
+//     res.redirect("/login");
+//   } catch (error) {
+//     res.redirect("/register");
+//   }
+// });
+
+// app.get("/register", (req, res) =>
+//   res.render("register.ejs", { message: req.flash("error") })
+// );
+
+app.get("/", (req, res) => {
+  res.send("welcome to jwt Route");
+});
+
+let posts = [
+  { username: "faris", age: "32" },
+  {
+    username: "amir",
+    age: "22",
+  },
+];
+let frist = Array.from(posts.values());
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  let user = { name: username };
+  let accessTokens = jwt.sign(user, secret);
+
+  console.log("accessTokens:", accessTokens);
+});
+
+let port = 9600;
+app.listen(port, () => {
+  console.log(`Server Running on https://localhost:${port}`);
+});
