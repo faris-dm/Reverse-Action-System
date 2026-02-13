4bc3b7b084729e19e03cec9581b2fa00b08999c0
faris Naser <solonaser9@gmail.com>
2026-02-13 08:21:16 -0800

    feat:added  a registor route to acept the user info  and store it in the  store

--- Server/simpleJwt2.js
+++ Server/simpleJwt2.js
@@ -11,13 +11,6 @@ const { populate } = require("dotenv");
 const { token } = require("morgan");
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/", (req, res) => {
-//   res.render("index.ejs", { message: name });
-// });
-// app.get("/register", (req, res) => {
-//   res.render("register.ejs");
-// });
-
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
@@ -27,34 +20,6 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 let secret = "W$q4=25*8%v-}UV";
 let RefreshTokenSecret = "W%&7=-^#-v}XL";
-
-// app.post("/register", async (req, res) => {
-//   try {
-//     let { email, password } = req.body;
-//     let cleanEmail = email.trim().toLowerCase();
-//     if (userMapStore.has(cleanEmail)) {
-//       alert("email aready exist");
-
-//       return res.redirect("/register");
-//     }
-
-//     let hashedPassword = await bcrypt.hash(password, 10);
-//     userMapStore.set(email, {
-//       id: Date.now(),
-//       name: req.body.name,
-//       email: email,
-//       password: hashedPassword,
-//     });
-//     console.log("stored succesfully", userMapStore.get(email));
-//     res.redirect("/login");
-//   } catch (error) {
-//     res.redirect("/register");
-//   }
-// });
-
-// app.get("/register", (req, res) =>
-//   res.render("register.ejs", { message: req.flash("error") })
-// );
 
 app.get("/", (req, res) => {
   res.send("welcome to jwt Route in the second server ");
@@ -67,7 +32,35 @@ let posts = [
     age: "22",
   },
 ];
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    let hashPassword = await bcrypt.hash(password, 10);
+
+    if (userMapStore.has(email)) {
+      res.send("email aready  found  with this emil");
+    }
+    userMapStore.get(email, {
+      id: Date.now().toString(),
+      name: name,
+      email: email,
+      password: hashPassword,
+    });
+    console.log("user added succefully", userMapStore.get(email));
+    res.redirect("/login");
+  } catch (error) {
+    console.log("error happened", error);
+    res.redirect("register");
+  }
+});
+
 let refreshStore = [];
+
 app.post("/token", (req, res) => {
   let RefreshTokens = req.body.token;
   if (!RefreshTokens) {
@@ -85,18 +78,6 @@ app.post("/token", (req, res) => {
   });
 });
 
-// app.get("/post", (req, res) => {
-//   // Use console.log to debug what is inside the token!
-//   console.log("Logged in user from token:", req.user);
-//   let allEmements = Array.from(posts.values());
-//   const filteredPosts = allEmements.filter((item) => {
-//     return item.username === req.user.username;
-//   });
-
-// res.json(filteredPosts);
-// });
-let frist = Array.from(posts.values());
-
 app.post("/login", (req, res) => {
   let username = req.body.username;
   let user = { username: username };
@@ -106,22 +87,6 @@ app.post("/login", (req, res) => {
   console.log("accessTokens:", accessTokens);
   res.json({ accessTokens: accessTokens, RefreshTokens: RefreshTokens });
 });
-
-// function tokenAuth(req, res, next) {
-//   let authHeader = req.headers["authorization"];
-//   let tokens = authHeader && authHeader.split(" ")[1];
-//   if (!tokens) {
-//     return res.send("Token does not found");
-//   }
-//   jwt.verify(tokens, secret, (err, user) => {
-//     if (err) {
-//       res.clearCookie("tokens");
-//       return res.redirect("/login");
-//     }
-//     req.user = user;
-//     next();
-//   });
-// }
 
 app.delete("/logout", (req, res) => {
   refreshStore = refreshStore.filter((token) => token !== req.body.token);
