da489168598f35e2bf4586c06e9eb71eb2e6bf7c
faris Naser <solonaser9@gmail.com>
2026-02-16 08:38:38 -0800

    feat:added  rules that govern the input from the form and  validateit

--- Server/routes/buyer.js
+++ Server/routes/buyer.js
@@ -17,4 +17,48 @@ let signSupplier = z.object({
 
 // POST /buyer – handle buyer registration
 
+router.post("/buyer", async (req, res) => {
+  let zodSupplierSign = signSupplier.safeParse(req.body);
+  if (!zodSupplierSign.success) {
+    return res.json({
+      err: zodSupplierSign.error.errors.map((errors) => ({
+        field: errors.path[0],
+        message: errors.message,
+      })),
+    });
+  }
+  // TODO: Save buyer data to database
+  let { name, email, password, confirmPassword, shippingAddress } = req.body;
+
+  try {
+    //  check the  password confiramation
+    if (password !== confirmPassword) {
+      return res.status(403).send("Password does not match");
+    }
+    console.log("Password Match Correctly");
+
+    let hashPassword = await bcrypt.hash(password, 10);
+    let cleanEmail = email.toLowerCase();
+    if (userMapStore.has(cleanEmail)) {
+      return res.status(403).send("email aready Found");
+    }
+    userMapStore.set(cleanEmail, {
+      id: Date.now(),
+      name: name,
+      email: email,
+      password: hashPassword,
+      shippingAddress: shippingAddress,
+    });
+    console.log("user supplier added succefully", userMapStore.get(cleanEmail));
+    // succusess redirec to login
+    return res.redirect("/login");
+  } catch (error) {
+    console.log("error happened", error);
+    console.log("error Happens");
+  }
+
+  // After successful registration, redirect to profile completion
+  //   res.redirect("/profile/complete?role=buyer");
+});
+
 module.exports = router;
