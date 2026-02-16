2caa100042e0a9b3d5e07561b0b1e9662a620431
faris Naser <solonaser9@gmail.com>
2026-02-16 05:57:46 -0800

    fix: refactor the supplier user information and make it store it info in the storage

--- Server/routes/supplier.js
+++ Server/routes/supplier.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { z, email } = require("zod");
+const { z, email, date } = require("zod");
 let userMapStore = new Map();
+const bcrypt = require("bcrypt");
 
 router.get("/supplier", (req, res) => {
   res.render("supplierRegistor", { message: null });
@@ -17,7 +18,7 @@ let signUpsupplier = z.object({
 router.post("/supplier", async (req, res) => {
   // save logic...
   let RegistorSuppleZod = signUpsupplier.safeParse(req.body);
-  if (!RegistorSuppleZod) {
+  if (!RegistorSuppleZod.success) {
     return res.status(400).json({
       err: RegistorSuppleZod.error.errors.map((errors) => ({
         field: errors.path[0],
@@ -26,34 +27,35 @@ router.post("/supplier", async (req, res) => {
     });
   }
 
-  const { email, password, name, businessName, confirmPassword } = req.body;
+  let { email, password, name, businessName, confirmPassword } = req.body;
   try {
+    if (password !== confirmPassword) {
+      return res.status(403).send("Password does not match");
+    }
+    console.log("Password Match Correctly");
+
     let hashPassword = await bcrypt.hash(password, 10);
     let cleanEmail = email.toLowerCase();
-    if (!userMapStore.has(cleanEmail)) {
+    if (userMapStore.has(cleanEmail)) {
       return res.status(403).send("email aready Found");
     }
+    confirmPassword = await bcrypt.hash(confirmPassword, 10);
 
-    if (!(await bcrypt.compare(password, confirmPassword))) {
-      console.log("incorrect password");
-      return res.json("incorrect Password");
-    }
-
-    userMapStore.set({
+    userMapStore.set(cleanEmail, {
+      id: Date.now(),
       name: name,
       email: email,
       password: hashPassword,
-      confirmPassword: hashPassword,
       businessName: businessName,
     });
-    console.log("user supplier added succefully", userMapStore.get(email));
+    console.log("user supplier added succefully", userMapStore.get(cleanEmail));
     return res.redirect("/");
   } catch (error) {
     console.log("error happened", error);
-    res.redirect("/role");
+    console.log("error Happens");
   }
 
-  res.redirect("/profile/complete?role=supplier");
+  //   res.redirect("/profile/complete?role=supplier");
 });
 
 module.exports = router;
