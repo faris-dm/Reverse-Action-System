ca05e67db51bdd3fa53a57b5f613786ed203da84
faris Naser <solonaser9@gmail.com>
2026-02-16 04:11:32 -0800

    feat:added a zod rules that govern how the user insert the info and respond based on it

--- Server/routes/supplier.js
+++ Server/routes/supplier.js
@@ -1,13 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const { z, email } = require("zod");
+let userMapStore = new Map();
 
 router.get("/supplier", (req, res) => {
   res.render("supplierRegistor", { message: null });
 });
 
+let signUpsupplier = z.object({
+  name: z.string().min(3, "userName Must Be at least three characters "),
+  email: z.string().email("pleases enter Valid email(@"),
+  password: z.string().min(5, "password must be five or more"),
+});
+
 // POST /supplier – handle form submission
-router.post("/supplier", (req, res) => {
+router.post("/supplier", async (req, res) => {
   // save logic...
+  let RegistorSuppleZod = signUpsupplier.safeParse(req.body);
+  if (!RegistorSuppleZod) {
+    return res.status(400).json({
+      err: RegistorSuppleZod.error.errors.map((errors) => ({
+        field: errors.path[0],
+        message: errors.message,
+      })),
+    });
+  }
+
+  const { email, password, name, businessName, confirmPassword } = req.body;
+  try {
+    let hashPassword = await bcrypt.hash(password, 10);
+    let cleanEmail = email.toLowerCase();
+    if (!userMapStore.has(cleanEmail)) {
+      return res.status(403).send("email aready Found");
+    }
+    userMapStore.set({
+      name: name,
+      email: email,
+      password: hashPassword,
+      businessName: businessName,
+    });
+    console.log("user supplier added succefully", userMapStore.get(email));
+    return res.redirect("/");
+  } catch (error) {
+    console.log("error happened", error);
+    res.redirect("/role");
+  }
 
   res.redirect("/profile/complete?role=supplier");
 });
