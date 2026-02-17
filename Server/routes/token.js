c13af4a52db4f3043050043d47e20a6ea45d3f52
faris Naser <solonaser9@gmail.com>
2026-02-16 17:32:20 -0800

    refactor the  token route  from main server   for better debuging  and  usability

---
+++ Server/routes/token.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const { z, email } = require("zod");
+let refreshStore = [];
+const userMapStore = require("../models/storeage");
+let RefreshTokenSecret = "W%&7=-^#-v}XL";
+
+router.post("/token", (req, res) => {
+  let authHeaderToken = req.body.token;
+  if (!authHeaderToken) {
+    res.status(401).send("No refresh Token found");
+  } else if (!refreshStore.includes(authHeaderToken)) {
+    res.status(401).send("Token does not much");
+  }
+  jwt.verify(authHeaderToken, RefreshTokenSecret, (err, user) => {
+    if (err) return res.status(403).send("Error happend Pleases check again");
+    let playload = {
+      email: user.email,
+      name: user.name,
+    };
+    let accessTokens = generateAccess(playload);
+    res.json({ accessTokens: accessTokens });
+  });
+});
+
+function generateAccess(user) {
+  return jwt.sign(user, secret, { expiresIn: "15m" });
+}
