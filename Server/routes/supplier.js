310e23ec8da8345eb8aa5679279695079d454bb6
faris Naser <solonaser9@gmail.com>
2026-02-18 05:43:33 -0800

    feat:created a function that create a token  when the user  registor

--- Server/routes/supplier.js
+++ Server/routes/supplier.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const { z, email, date } = require("zod");
 const userMapStore = require("../models/storeage");
 const bcrypt = require("bcrypt");
+const userMap = require("../models/storeage");
+let secret = "W$q4=25*8%v-}UV";
+let jwt = require("jsonwebtoken");
+let RefreshTokenSecret = "W%&7=-^#-v}XL";
+let cookiesparser = require("cookie-parser");
+router.use(cookiesparser());
 
 router.get("/supplier", (req, res) => {
   res.render("supplierRegistor", { message: null });
@@ -27,7 +33,7 @@ router.post("/supplier", async (req, res) => {
     });
   }
 
-  let { email, password, name, businessName, confirmPassword } = req.body;
+  let { email, password, name, businessName, role, confirmPassword } = req.body;
   try {
     if (password !== confirmPassword) {
       return res.status(403).send("Password does not match");
@@ -36,6 +42,7 @@ router.post("/supplier", async (req, res) => {
 
     let hashPassword = await bcrypt.hash(password, 10);
     let cleanEmail = email.toLowerCase();
+    let FoundUser = userMap.get(cleanEmail);
     if (userMapStore.has(cleanEmail)) {
       return res.status(403).send("email aready Found");
     }
@@ -48,14 +55,28 @@ router.post("/supplier", async (req, res) => {
       password: hashPassword,
       businessName: businessName,
     });
+
+    let user = { email: cleanEmail, name: name, role: role };
+
+    let accessTokens = generateAccesToken(user);
+
+    let RefreshTokens = jwt.sign(user, RefreshTokenSecret, { expiresIn: "7d" });
+    res.cookie("accessTokens", accessTokens, { httpOnly: true });
+    res.cookie("RefreshTokens", RefreshTokens, { httpOnly: true });
+
+    console.log("token created succefully");
+
     console.log("user supplier added succefully", userMapStore.get(cleanEmail));
-    return res.redirect("/");
+    return res.redirect("/Dashboard");
   } catch (error) {
     console.log("error happened", error);
-    console.log("error Happens");
   }
 
   //   res.redirect("/profile/complete?role=supplier");
 });
+
+function generateAccesToken(user) {
+  return jwt.sign(user, secret, { expiresIn: "10m" });
+}
 
 module.exports = router;
