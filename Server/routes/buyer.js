cb4a13834dab70a8f09f5e427bfb0ac55e00bb08
faris Naser <solonaser9@gmail.com>
2026-02-18 18:08:26 -0800

    fix: the zod rule , added a role in the jwt ,changed the tokens  name and added status code

--- Server/routes/buyer.js
+++ Server/routes/buyer.js
@@ -7,14 +7,14 @@ let jwt = require("jsonwebtoken");
 let secret = "W$q4=25*8%v-}UV";
 let RefreshTokenSecret = "W%&7=-^#-v}XL";
 let cookiesparser = require("cookie-parser");
-router.use(cookiesparser());
+// router.use(cookiesparser());
 
 // GET /buyer – show buyer registration form
 router.get("/buyer", (req, res) => {
   res.render("buyerRegister", { message: null });
 });
 
-let signSupplier = z.object({
+let signBuyer = z.object({
   name: z.string().min(3, "Username Must Be more than Two words"),
   email: z.string().email("please enter Valid email- @"),
   password: z.string().min(5, "password must be five or more"),
@@ -23,54 +23,56 @@ let signSupplier = z.object({
 // POST /buyer – handle buyer registration
 
 router.post("/buyer", async (req, res) => {
-  let zodSupplierSign = signSupplier.safeParse(req.body);
-  if (!zodSupplierSign.success) {
-    return res.json({
-      err: zodSupplierSign.error.errors.map((errors) => ({
+  let result = signBuyer.safeParse(req.body);
+  if (!result.success) {
+    return res.status(400).json({
+      err: result.error.errors.map((errors) => ({
         field: errors.path[0],
         message: errors.message,
       })),
     });
   }
   // TODO: Save buyer data to database
-  let { name, email, password, role, confirmPassword, shippingAddress } =
-    req.body;
+  let { name, email, password, confirmPassword, shippingAddress } = req.body;
+  let role = "buyer";
 
   try {
     //  check the  password confiramation
     if (password !== confirmPassword) {
-      return res.status(403).send("Password does not match");
+      return res.status(400).send("Password does not match");
     }
     console.log("Password Match Correctly");
 
     let hashPassword = await bcrypt.hash(password, 10);
     let cleanEmail = email.toLowerCase();
     if (userMapStore.has(cleanEmail)) {
-      return res.status(403).send("email aready Found");
+      return res.status(400).send("email aready Found");
     }
     userMapStore.set(cleanEmail, {
       id: Date.now(),
       name: name,
       email: email,
+      role: role,
       password: hashPassword,
       shippingAddress: shippingAddress,
     });
 
     let user = { email: cleanEmail, name: name, role: role };
 
-    let accessTokens = generateAccesToken(user);
-    let RefreshTokens = jwt.sign(user, RefreshTokenSecret, {
+    let token = generateAccesToken(user);
+    let RefreshToken = jwt.sign(user, RefreshTokenSecret, {
       expiresIn: "7d",
     });
 
-    res.cookie("accessTokens", accessTokens, { httpOnly: true });
-    res.cookie("refreshTokens", RefreshTokens, { httpOnly: true });
+    res.cookie("accessToken", token, { httpOnly: true });
+    res.cookie("RefreshToken", RefreshToken, { httpOnly: true });
 
     console.log("user buyer added succefully", userMapStore.get(cleanEmail));
     // succusess redirec to login
-    return res.redirect("/login");
+    return res.redirect("/Dashboard");
   } catch (error) {
     console.log("error happened", error);
+    return res.status(500).send("Error happens");
   }
 
   // After successful registration, redirect to profile completion
