f618125acf2bb1090b95403745b676cd6562504f
faris Naser <solonaser9@gmail.com>
2026-02-18 18:08:31 -0800

    remove the cookies for unversal use,added status code, fix mesages

--- Server/routes/supplier.js
+++ Server/routes/supplier.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const { z, email, date } = require("zod");
 const userMapStore = require("../models/storeage");
 const bcrypt = require("bcrypt");
-const userMap = require("../models/storeage");
+// const userMap = require("../models/storeage");
 let secret = "W$q4=25*8%v-}UV";
 let jwt = require("jsonwebtoken");
 let RefreshTokenSecret = "W%&7=-^#-v}XL";
-let cookiesparser = require("cookie-parser");
-router.use(cookiesparser());
+// let cookiesparser = require("cookie-parser");
+// router.use(cookiesparser());
 
 router.get("/supplier", (req, res) => {
   res.render("supplierRegistor", { message: null });
@@ -23,6 +23,7 @@ let signUpsupplier = z.object({
 // POST /supplier – handle form submission
 router.post("/supplier", async (req, res) => {
   // save logic...
+
   let RegistorSuppleZod = signUpsupplier.safeParse(req.body);
   if (!RegistorSuppleZod.success) {
     return res.status(400).json({
@@ -33,36 +34,39 @@ router.post("/supplier", async (req, res) => {
     });
   }
 
-  let { email, password, name, businessName, role, confirmPassword } = req.body;
+  let { email, password, name, businessName, confirmPassword } = req.body;
+  let role = "supplier";
+
   try {
     if (password !== confirmPassword) {
-      return res.status(403).send("Password does not match");
+      return res.status(400).send("Password does not match");
     }
     console.log("Password Match Correctly");
 
     let hashPassword = await bcrypt.hash(password, 10);
     let cleanEmail = email.toLowerCase();
-    let FoundUser = userMap.get(cleanEmail);
+    // let FoundUser = userMap.get(cleanEmail);
     if (userMapStore.has(cleanEmail)) {
-      return res.status(403).send("email aready Found");
+      return res.status(400).send("email aready Found");
     }
-    confirmPassword = await bcrypt.hash(confirmPassword, 10);
+    // confirmPassword = await bcrypt.hash(confirmPassword, 10);
 
     userMapStore.set(cleanEmail, {
       id: Date.now(),
       name: name,
       email: email,
+      role: role,
       password: hashPassword,
       businessName: businessName,
     });
 
     let user = { email: cleanEmail, name: name, role: role };
 
-    let accessTokens = generateAccesToken(user);
+    let token = generateAccesToken(user);
 
-    let RefreshTokens = jwt.sign(user, RefreshTokenSecret, { expiresIn: "7d" });
-    res.cookie("accessTokens", accessTokens, { httpOnly: true });
-    res.cookie("RefreshTokens", RefreshTokens, { httpOnly: true });
+    let RefreshToken = jwt.sign(user, RefreshTokenSecret, { expiresIn: "7d" });
+    res.cookie("accessTokens", token, { httpOnly: true });
+    res.cookie("RefreshTokens", RefreshToken, { httpOnly: true });
 
     console.log("token created succefully");
 
@@ -70,6 +74,7 @@ router.post("/supplier", async (req, res) => {
     return res.redirect("/Dashboard");
   } catch (error) {
     console.log("error happened", error);
+    return res.send("Error happens ");
   }
 
   //   res.redirect("/profile/complete?role=supplier");
