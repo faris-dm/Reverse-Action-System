9359f7a0a7e684c161b755f4f1d2df74860bf13f
faris Naser <solonaser9@gmail.com>
2026-02-21 04:48:01 -0800

    created new logout page  that clear the cookies

--- Server/routes/logout.js
+++ Server/routes/logout.js
@@ -2,9 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/logout", (req, res) => {
-  res.clearCookie("token");
-  res.clearCookie("refreshToken");
-  console.log("User logged out, cookies cleared");
+  const token = req.cookies.token; // you can decode it to get the user email
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, secret);
+      const user = userMapStore.get(decoded.email);
+      if (user) {
+        delete user.refreshToken; // or set to null
+        userMapStore.set(decoded.email, user);
+      }
+    } catch (err) {
+      // token might be invalid – ignore
+    }
+  }
+
   res.redirect("/role");
 });
 
