653e36375524542fb62a0ee2f65c0d335a1f8163
faris Naser <solonaser9@gmail.com>
2026-03-19 05:58:06 -0700

    inported the registor form in the main

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,26 +5,29 @@ import Supplier from "./pages/supplier";
 import Admin from "./pages/admin";
 import NewBuyer from "./pages/newBuyer";
 import Hero from "./pages/Hero";
-import NewHero from "./pages/newHero";
+import MainHero from "./pages/newHero";
 import Database from "./pages/database";
 import SupplierFactor from "./components/supplier/supplier";
 import Role from "./pages/Role";
 import LoginPage from "./components/ui/Login";
+import SupplierRegitor from "./components/Registor/supplierRegistor";
 
 function App() {
   return (
     <div>
-      <Supplier />
       {/* <SupplierFactor /> */}
-      {/* <Buyer /> */}
       {/* <Admin /> */}
-      {/* <NewBuyer /> */}
-      {/* <NewHero /> */}
-      {/* <LoginPage /> */}
-
-      {/* <Role /> */}
-      {/* <Database /> */}
+      {/*     <MainHero />  */}
+      {/*   <LoginPage /> */}
+      {/*   <Role />   */}
       {/* <Hero /> */}
+      {/*    */}
+      {/* <SupplierRegitor /> */}
+      // // // //
+      llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+      <Buyer />
+      {/* <NewBuyer /> */}
+      {/*  <Supplier /> */}
     </div>
   );
 }
