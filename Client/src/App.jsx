6699dbdd3ee6a857de42c026f57970f8b67ff042
faris Naser <solonaser9@gmail.com>
2026-03-22 16:16:16 -0700

    added  react route  to navigate between pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+
+import { BrowserRouter, Router, Route, Link, Routes } from "react-router-dom";
 // import Supplier from "./pages/supplier";
 import Buyer from "./pages/buyer";
 import Supplier from "./pages/supplier";
@@ -11,23 +13,50 @@ import SupplierFactor from "./components/supplier/supplier";
 import Role from "./pages/Role";
 import LoginPage from "./components/ui/Login";
 import SupplierRegitor from "./components/Registor/supplierRegistor";
+// import mainBuyer from "./pages/mainBuyer";
+import MainBuyer from "./pages/mainBuyer";
+import BuyerRegistor from "./components/Registor/buyerRegistor";
 
 function App() {
   return (
     <div>
-      {/* <SupplierFactor /> */}
-      {/* <Admin /> */}
-      {/*     <MainHero />  */}
-      {/*   <LoginPage /> */}
-      {/*   <Role />   */}
-      {/* <Hero /> */}
-      {/*    */}
-      {/* <SupplierRegitor /> */}
-      // // // //
-      llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
-      <Buyer />
-      {/* <NewBuyer /> */}
-      {/*  <Supplier /> */}
+      <div>
+        {/* <SupplierFactor /> */}
+        {/* <Admin /> */}
+        {/* <MainHero /> */}
+        {/*   <LoginPage /> */}
+        {/*   <Role />   */}
+        {/* <Hero /> */}
+        {/*    <SupplierRegitor />  */}
+        {/* <BuyerRegistor /> */}
+        {/* // <mainBuyer /> */}
+        {/* <MainBuyer /> */}
+        {/* // // // // */}
+        {/* llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll */}
+        {/* <Buyer /> */}
+        {/* <NewBuyer /> */}
+        {/*  <Supplier /> */}
+      </div>
+      <BrowserRouter>
+        <Routes>
+          {/* landing page */}
+          <Route path="/" element={<Hero />} />
+          {/*  page  for desideing the role */}
+          <Route path="/role" element={<Role />} />
+          {/* login  page */}
+          <Route path="/login" element={<LoginPage />} />
+          {/* Supplier registor */}
+          <Route path="/supplerform" element={<SupplierRegitor />} />
+          {/* Buyer Registor */}
+          <Route path="/buyerform" element={<BuyerRegistor />} />
+          {/* supplier  Dashboard */}
+          <Route path="/supplier" element={<SupplierFactor />} />
+          {/* buyer Dashboard */}
+          <Route path="/buyer" element={<MainBuyer />} />
+          {/* dashboard  for the admin */}
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
