f39ec996818042caf5298ae5550d2c6ac19e0723
faris Naser <solonaser9@gmail.com>
2026-03-28 17:10:19 -0700

    acheme   for  the database

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,19 +1,19 @@
-const { Pool, Client } = require("pg");
-const connect = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "new",
-  password: "admin",
-  port: 5432,
-});
+// const { Pool, Client } = require("pg");
+// const connect = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "new",
+//   password: "admin",
+//   port: 5432,
+// });
 
-connect.connect((error, client, release) => {
-  if (error) {
-    console.error("connection Failed", error.message);
-  } else {
-    console.log("connection  succefully");
-    release();
-  }
-});
+// connect.connect((error, client, release) => {
+//   if (error) {
+//     console.error("connection Failed", error.message);
+//   } else {
+//     console.log("connection  succefully");
+//     release();
+//   }
+// });
 
-module.exports = connect;
+// module.exports = connect;
