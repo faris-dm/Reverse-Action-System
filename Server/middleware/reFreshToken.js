6d5254a7360092445b0f0206d433671a1d2b6b59
faris Naser <solonaser9@gmail.com>
2026-04-09 05:32:48 -0700

    created file to verify the refreshtokens and generate the access token in the way

---
+++ Server/middleware/reFreshToken.js
@@ -0,0 +1,72 @@
+const express=require("express")
+router.use(cookieParser());
+let router=express.Router()
+const jwt = require("jsonwebtoken");
+router.use(cookiesparser());
+const UserStorage = require("../models/storeage");
+const cookieParser = require("cookie-parser");
+
+
+let secret = "W$q4=25*8%v-}UV";
+let RefreshTokenSecret = "W%&7=-^#-v}XL";
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+
+
+function AuthREFreshToken(req,res,next) {
+    const oldRefreshtoken = req.cookies.refreshToken;
+
+    if (!oldRefreshtoken) {
+      return res.satus(401).json({
+        success: false,
+        message: "session not found ",
+      });
+    }
+jwt.verify(oldRefreshtoken, RefreshTokenSecret, (err, decoded) => {
+  if (err) {
+    return res.status(401).json("RefreshTokens are  EXPRED");
+  }
+
+  let storegeTokens = UserStorage.get(decoded.email);
+  if (!storegeTokens) {
+    return res.satus(401).json("There is no this toekn in the databse");
+  }
+
+  if (storegeTokens.RefreshToken !== oldRefreshtoken) {
+    return res.status(401).json("there is no similarity in the tokens");
+  }
+  let accessToken = jwt.sign(
+    {
+        id:storegeTokens.id,
+      email: storegeTokens.email,
+      role:storegeTokens.role
+    },
+    secret,
+    { expiresIn: "15m" }
+  );
+
+  let refreshToken = jwt.sign({email:decoded.email}, RefreshTokenSecret, {
+    expiresIn: "7d",
+  });
+  res.cookie("token", accessToken, {
+    httpOnly: true,
+    maxAge: 15 * 60 * 1000,
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  storegeTokens.refreshToken=refreshToken
+  UserStorage.set(storegeTokens.email, storegeTokens);
+
+  next();
+});
+
+}
+
+
+
+module.exports =AuthREFreshToken
